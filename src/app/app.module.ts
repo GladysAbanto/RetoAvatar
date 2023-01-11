86a0276397525b96873945d6580ed4df77cb4e97
GladysAZ <gladysabanto99@gmail.com>
2023-01-11 17:35:13 -0500

    Se crea el diseño del componente film

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FilmsComponent } from './pages/films/films.component';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FilmComponent } from './components/film/film.component';
 
 @NgModule({
-  declarations: [AppComponent, FilmsComponent, CharactersComponent],
+  declarations: [AppComponent, FilmsComponent, CharactersComponent, FilmComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
